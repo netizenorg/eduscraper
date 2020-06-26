2630317332cd21f98e570b71670188c32d6882f2
nbriz <nickbriz@gmail.com>
2020-06-26 16:20:47 -0500

    converted to npm module

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const setting = process.argv[2]
-const destination = process.argv[3]
+#!/usr/bin/env node
+
+const setting = process.argv[2] || 'all'
+const destination = process.argv[3] || __dirname
 const scrapeHTMLAttributeNFO = require('./src/scrapeHTMLAttributeNFO.js')
 const scrapeHTMLElementsNFO = require('./src/scrapeHTMLElementsNFO.js')
+const scrapeCSSPropertiesNFO = require('./src/scrapeCSSPropertiesNFO.js')
 
 if (setting === 'all' || setting === 'attributes') {
   scrapeHTMLAttributeNFO(destination, (err) => console.log('ERROR:', err))
@@ -9,4 +12,8 @@ if (setting === 'all' || setting === 'attributes') {
 
 if (setting === 'all' || setting === 'elements') {
   scrapeHTMLElementsNFO(destination, (err) => console.log('ERROR:', err))
+}
+
+if (setting === 'all' || setting === 'properties') {
+  scrapeCSSPropertiesNFO(destination, (err) => console.log('ERROR:', err))
 }
