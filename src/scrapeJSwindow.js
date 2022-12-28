0f04d33327806ef765ab652479e21698526bf171
nbriz <nickbriz@gmail.com>
2022-12-28 01:13:40 -0600

    updated js-window logic

--- src/scrapeJSwindow.js
+++ src/scrapeJSwindow.js
@@ -16,12 +16,18 @@ function scrapeJSRefDescription (data) {
   return description
 }
 
+// on plain .html page
 const props = [ // Object.keys(window).join("', '")
   'close', 'stop', 'focus', 'blur', 'open', 'alert', 'confirm', 'prompt', 'print', 'postMessage', 'captureEvents', 'releaseEvents', 'getSelection', 'getComputedStyle', 'matchMedia', 'moveTo', 'moveBy', 'resizeTo', 'resizeBy', 'scroll', 'scrollTo', 'scrollBy', 'getDefaultComputedStyle', 'scrollByLines', 'scrollByPages', 'sizeToContent', 'updateCommands', 'find', 'dump', 'setResizable', 'requestIdleCallback', 'cancelIdleCallback', 'requestAnimationFrame', 'cancelAnimationFrame', 'reportError', 'btoa', 'atob', 'setTimeout', 'clearTimeout', 'setInterval', 'clearInterval', 'queueMicrotask', 'createImageBitmap', 'structuredClone', 'fetch', 'self', 'name', 'history', 'customElements', 'locationbar', 'menubar', 'personalbar', 'scrollbars', 'statusbar', 'toolbar', 'status', 'closed', 'event', 'frames', 'length', 'opener', 'parent', 'frameElement', 'navigator', 'clientInformation', 'external', 'applicationCache', 'screen', 'innerWidth', 'innerHeight', 'scrollX', 'pageXOffset', 'scrollY', 'pageYOffset', 'screenLeft', 'screenTop', 'screenX', 'screenY', 'outerWidth', 'outerHeight', 'performance', 'mozInnerScreenX', 'mozInnerScreenY', 'devicePixelRatio', 'scrollMaxX', 'scrollMaxY', 'fullScreen', 'ondevicemotion', 'ondeviceorientation', 'onabsolutedeviceorientation', 'InstallTrigger', 'visualViewport', 'crypto', 'onabort', 'onblur', 'onfocus', 'onauxclick', 'onbeforeinput', 'oncanplay', 'oncanplaythrough', 'onchange', 'onclick', 'onclose', 'oncontextmenu', 'oncuechange', 'ondblclick', 'ondrag', 'ondragend', 'ondragenter', 'ondragexit', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'ondurationchange', 'onemptied', 'onended', 'onformdata', 'oninput', 'oninvalid', 'onkeydown', 'onkeypress', 'onkeyup', 'onload', 'onloadeddata', 'onloadedmetadata', 'onloadstart', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onwheel', 'onpause', 'onplay', 'onplaying', 'onprogress', 'onratechange', 'onreset', 'onresize', 'onscroll', 'onsecuritypolicyviolation', 'onseeked', 'onseeking', 'onselect', 'onslotchange', 'onstalled', 'onsubmit', 'onsuspend', 'ontimeupdate', 'onvolumechange', 'onwaiting', 'onselectstart', 'onselectionchange', 'ontoggle', 'onpointercancel', 'onpointerdown', 'onpointerup', 'onpointermove', 'onpointerout', 'onpointerover', 'onpointerenter', 'onpointerleave', 'ongotpointercapture', 'onlostpointercapture', 'onmozfullscreenchange', 'onmozfullscreenerror', 'onanimationcancel', 'onanimationend', 'onanimationiteration', 'onanimationstart', 'ontransitioncancel', 'ontransitionend', 'ontransitionrun', 'ontransitionstart', 'onwebkitanimationend', 'onwebkitanimationiteration', 'onwebkitanimationstart', 'onwebkittransitionend', 'u2f', 'onerror', 'speechSynthesis', 'onafterprint', 'onbeforeprint', 'onbeforeunload', 'onhashchange', 'onlanguagechange', 'onmessage', 'onmessageerror', 'onoffline', 'ononline', 'onpagehide', 'onpageshow', 'onpopstate', 'onrejectionhandled', 'onstorage', 'onunhandledrejection', 'onunload', 'ongamepadconnected', 'ongamepaddisconnected', 'localStorage', 'origin', 'crossOriginIsolated', 'isSecureContext', 'indexedDB', 'caches', 'sessionStorage', 'window', 'document', 'location', 'top'
 ]
 
+// on https://developer.mozilla.org/en-US/docs/Web/API/Window
+const mdn = [ // [...document.querySelectorAll('dt > a > code')].map(e => e.textContent).filter(t => t.indexOf('Window') === 0).map(s => s.substr(7)).join("', '")
+  'clientInformation', 'closed', 'console', 'credentialless', 'customElements', 'crypto', 'devicePixelRatio', 'document', 'event', 'external', 'frameElement', 'frames', 'fullScreen', 'history', 'innerHeight', 'innerWidth', 'length', 'location', 'locationbar', 'localStorage', 'menubar', 'messageManager', 'mozInnerScreenX', 'mozInnerScreenY', 'name', 'navigation', 'navigator', 'opener', 'outerHeight', 'outerWidth', 'pageXOffset', 'pageYOffset', 'parent', 'performance', 'personalbar', 'screen', 'screenX', 'screenLeft', 'screenY', 'screenTop', 'scrollbars', 'scrollMaxX', 'scrollMaxY', 'scrollX', 'scrollY', 'self', 'sessionStorage', 'sidebar', 'speechSynthesis', 'status', 'statusbar', 'toolbar', 'top', 'visualViewport', 'window', 'scheduler', 'content', 'defaultStatus', 'orientation', 'returnValue', 'alert', 'blur', 'cancelAnimationFrame', 'cancelIdleCallback', 'clearImmediate', 'close', 'confirm', 'dump', 'find', 'focus', 'getComputedStyle', 'getDefaultComputedStyle', 'getSelection', 'matchMedia', 'moveBy', 'moveTo', 'open', 'postMessage', 'print', 'prompt', 'queryLocalFonts', 'requestAnimationFrame', 'requestIdleCallback', 'resizeBy', 'resizeTo', 'scroll', 'scrollBy', 'scrollByLines', 'scrollByPages', 'scrollTo', 'setImmediate', 'setResizable', 'sizeToContent', 'showOpenFilePicker', 'showSaveFilePicker', 'showDirectoryPicker', 'stop', 'updateCommands', 'back', 'captureEvents', 'forward', 'home', 'releaseEvents', 'showModalDialog'
+]
+
 const filterOut = [
-  'InstallTrigger', 'clientInformation', 'setResizable', 'u2f', 'ontoggle', 'ondragexit', 'onabsolutedeviceorientation'
+  'InstallTrigger', 'clientInformation', 'setResizable', 'u2f', 'ontoggle', 'ondragexit', 'onabsolutedeviceorientation', 'returnValue', 'messageManager', 'home'
 ]
 
 const req = async (url) => {
@@ -94,7 +100,8 @@ async function getNfo (p) {
 
 async function scrapeJSnfo (url, file, destination, cb) {
   const dictionary = {}
-  const filtered = props
+  const both = [...props, ...mdn]
+  const filtered = [...new Set(both)]
     .filter(p => p.indexOf('onmoz') !== 0)
     .filter(p => p.indexOf('onwebkit') !== 0)
     .filter(p => !filterOut.includes(p))
@@ -115,6 +122,8 @@ async function scrapeJSnfo (url, file, destination, cb) {
       if (Object.keys(dictionary).length === filtered.length) {
         save(dictionary, `${destination}/${file}.json`)
         return dictionary
+      } else {
+        console.log(`... ${p}`)
       }
     }
   })
